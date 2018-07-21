fdce8fbb798fcd3c05aa4de3130131942368ee08
tylerburleigh <tylerburleigh@gmail.com>
2018-07-21 14:53:30 -0400

    5 Minute Stroop
    First commit

--- experiment.js
+++ experiment.js
@@ -5,6 +5,7 @@ function assessPerformance() {
 	var experiment_data = jsPsych.data.getTrialsOfType('poldrack-categorize')
 	var missed_count = 0
 	var trial_count = 0
+	var correct_count = 0
 	var rt_array = []
 	var rt = 0
 		//record choices participants made
@@ -18,7 +19,9 @@ function assessPerformance() {
 			trial_count += 1
 			rt = experiment_data[i].rt
 			key = experiment_data[i].key_press
+			correct = experiment_data[i].correct
 			choice_counts[key] += 1
+			if(correct) correct_count += 1
 			if (rt == -1) {
 				missed_count += 1
 			} else {
@@ -38,10 +41,23 @@ function assessPerformance() {
 			responses_ok = false
 		}
 	})
-	var missed_percent = missed_count/trial_count
-	credit_var = (missed_percent < 0.4 && avg_rt > 200 && responses_ok)
+	var missed_pct = missed_count/trial_count
+	var accuracy = correct_count/trial_count
+	var attn_correct_pct = evalAttentionChecks()
+	
+	credit_var = (missed_pct < 0.4 && avg_rt > 200 && responses_ok && accuracy > 0.6)
 	jsPsych.data.addDataToLastTrial({"credit_var": credit_var})
+	
+	results = {
+			missed_pct: missed_pct, 
+			accuracy: accuracy, 
+			attn_correct_pct: attn_correct_pct, 
+			credit_var: credit_var
+			};
+	
+	return(results);
 }
+
 
 function evalAttentionChecks() {
 	var check_percent = 1
@@ -67,15 +83,22 @@ var getInstructFeedback = function() {
 /* Define experimental variables */
 /* ************************************ */
 // generic task variables
-var run_attention_checks = false
+var run_attention_checks = true
 var attention_check_thresh = 0.45
 var sumInstructTime = 0 //ms
 var instructTimeThresh = 0 ///in seconds
 var credit_var = 0
 
+/*
+/* High contrast, color-blind safe colors
+/*	RED = #f64747
+/*	BLUE = #00bfff
+/*	YELLOW = #F1F227
+*/
+
 // task specific variables
 var congruent_stim = [{
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:red">RED</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#f64747">RED</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'congruent',
@@ -85,7 +108,7 @@ var congruent_stim = [{
 	},
 	key_answer: 82
 }, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:blue">BLUE</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#00bfff">BLUE</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'congruent',
@@ -94,20 +117,20 @@ var congruent_stim = [{
 		correct_response: 66
 	},
 	key_answer: 66
-}, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:green">GREEN</div></div>',
+},{
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#F1F227">YELLOW</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'congruent',
-		stim_color: 'green',
-		stim_word: 'green',
-		correct_response: 71
+		stim_color: 'yellow',
+		stim_word: 'yellow',
+		correct_response: 89
 	},
-	key_answer: 71
+	key_answer: 89
 }];
 
 var incongruent_stim = [{
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:red">BLUE</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#f64747">BLUE</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'incongruent',
@@ -117,17 +140,17 @@ var incongruent_stim = [{
 	},
 	key_answer: 82
 }, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:red">GREEN</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#f64747">YELLOW</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'incongruent',
 		stim_color: 'red',
-		stim_word: 'green',
+		stim_word: 'yellow',
 		correct_response: 82
 	},
 	key_answer: 82
 }, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:blue">RED</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#00bfff">RED</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'incongruent',
@@ -137,42 +160,43 @@ var incongruent_stim = [{
 	},
 	key_answer: 66
 }, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:blue">GREEN</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#00bfff">YELLOW</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'incongruent',
 		stim_color: 'blue',
-		stim_word: 'green',
+		stim_word: 'yellow',
 		correct_response: 66
 	},
 	key_answer: 66
 }, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:green">RED</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#F1F227">RED</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'incongruent',
-		stim_color: 'green',
+		stim_color: 'yellow',
 		stim_word: 'red',
-		correct_response: 71
+		correct_response: 89
 	},
-	key_answer: 71
+	key_answer: 89
 }, {
-	stimulus: '<div class = centerbox><div class = stroop-stim style = "color:green">BLUE</div></div>',
+	stimulus: '<div class = centerbox><div class = stroop-stim style = "font-weight:bold;color:#F1F227">BLUE</div></div>',
 	data: {
 		trial_id: 'stim',
 		condition: 'incongruent',
-		stim_color: 'green',
+		stim_color: 'yellow',
 		stim_word: 'blue',
-		correct_response: 71
+		correct_response: 89
 	},
-	key_answer: 71
+	key_answer: 89
 }];
-var stims = [].concat(congruent_stim, congruent_stim, incongruent_stim)
-var practice_len = 24
-var practice_stims = jsPsych.randomization.repeat(stims, practice_len / 12, true)
-var exp_len = 96
-var test_stims = jsPsych.randomization.repeat(stims, exp_len / 12, true)
-var choices = [66, 71, 82]
+// High proportion congruency: twice as many congruent as incongruent
+var stims = [].concat(congruent_stim, congruent_stim, congruent_stim, congruent_stim, incongruent_stim)
+var practice_len = 18
+var practice_stims = jsPsych.randomization.repeat(stims, practice_len / 18, true)
+var exp_len = 72
+var test_stims = jsPsych.randomization.repeat(stims, exp_len / 18, true)
+var choices = [66, 82, 89]
 var exp_stage = 'practice'
 
 /* ************************************ */
@@ -184,9 +208,9 @@ var attention_check_block = {
 	data: {
 		trial_id: "attention_check"
 	},
-	timing_response: 180000,
+	timing_response: 7500,
 	response_ends_trial: true,
-	timing_post_trial: 200
+	timing_post_trial: 1000
 }
 
 var attention_node = {
@@ -208,13 +232,14 @@ var post_task_block = {
    columns: [60,60]
 };
 
+
 /* define static blocks */
 var response_keys =
-	'<ul list-text><li><span class = "large" style = "color:red">WORD</span>: "R key"</li><li><span class = "large" style = "color:blue">WORD</span>: "B key"</li><li><span class = "large" style = "color:green">WORD</span>: "G key"</li></ul>'
+	'<ul class="list-text"><li><span class = "large" style = "color:#f64747;font-weight:bold">WORD</span>: "R key"</li><li><span class = "large" style = "color:#00bfff;font-weight:bold">WORD</span>: "B key"</li><li><span class = "large" style = "color:#F1F227;font-weight:bold">WORD</span>: "Y key"</li></ul>'
 
 
 var feedback_instruct_text =
-	'Welcome to the experiment. This experiment will take about 8 minutes. Press <strong>enter</strong> to begin.'
+	'<div class = centerbox><p class = block-text>Let\'s play a color matching game! Focus will be important here, so before we begin please make sure you\'re ready for <u><strong>five minutes</strong></u> of uninterrupted game time!</p> <p class = block-text>Press <strong>enter</strong> to continue.</p></div>'
 var feedback_instruct_block = {
 	type: 'poldrack-text',
 	data: {
@@ -225,6 +250,7 @@ var feedback_instruct_block = {
 	timing_post_trial: 0,
 	timing_response: 180000
 };
+
 /// This ensures that the subject does not read through the instructions too quickly.  If they do it too quickly, then we will go over the loop again.
 var instructions_block = {
 	type: 'poldrack-instructions',
@@ -232,8 +258,20 @@ var instructions_block = {
 		trial_id: "instruction"
 	},
 	pages: [
-		'<div class = centerbox><p class = block-text>In this experiment you will see "color" words (RED, BLUE, GREEN) appear one at a time. The "ink" of the words also will be colored. For example, you may see: <span class = "large" style = "color:blue">RED</span>, <span class = "large" style = "color:blue">BLUE</span> or <span class = "large" style = "color:red">BLUE</span>.</p><p class = block-text>Your task is to press the button corresponding to the <strong> ink color </strong> of the word. It is important that you respond as quickly and accurately as possible. The response keys are as follows:</p>' +
+		'<div class = centerbox style="height:80vh"><p class = block-text>In this game you will see "color" words (RED, BLUE, GREEN, YELLOW) appear one at a time. The "ink" of the words also will be colored. For example, you may see: <span class = "large" style = "color:#f64747;font-weight:bold">RED</span>, <span class = "large" style = "color:#00bfff;font-weight:bold">BLUE</span> or <span class = "large" style = "color:#f64747;font-weight:bold">BLUE</span>.</p><p class = block-text>Your task is to press the button corresponding to the <strong><u> font color </u></strong> of the word. Respond as <u><strong>quickly and accurately</strong></u> as possible. The response keys are as follows:</p>' +
 		response_keys + '</div>'
+	],
+	allow_keys: false,
+	show_clickable_nav: true,
+	timing_post_trial: 1000
+};
+var instructions_block2 = {
+	type: 'poldrack-instructions',
+	data: {
+		trial_id: "instruction2"
+	},
+	pages: [
+		'<div class = centerbox style="height:80vh"><p class = block-text>Place your fingers on the keyboard however is most comfortable to you. A suggested placement is shown below.</p><p><img src="recommended_finger_placement.svg" alt="Recommended finger placement diagram"></p></div>'
 	],
 	allow_keys: false,
 	show_clickable_nav: true,
@@ -241,7 +279,7 @@ var instructions_block = {
 };
 
 var instruction_node = {
-	timeline: [feedback_instruct_block, instructions_block],
+	timeline: [feedback_instruct_block, instructions_block, instructions_block2],
 	/* This function defines stopping criteria */
 	loop_function: function(data) {
 		for (i = 0; i < data.length; i++) {
@@ -261,6 +299,19 @@ var instruction_node = {
 	}
 }
 
+var check_performance  = {
+	type: 'poldrack-text',
+	data: {
+		trial_id: "end",
+    	exp_id: 'stroop'
+	},
+	timing_response: 180000,
+	text: '<div class = centerbox><p class = center-block-text>Thanks for playing!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
+	cont_key: [13],
+	timing_post_trial: 0,
+	on_finish: assessPerformance
+};
+
 var end_block = {
 	type: 'poldrack-text',
 	data: {
@@ -268,7 +319,7 @@ var end_block = {
     	exp_id: 'stroop'
 	},
 	timing_response: 180000,
-	text: '<div class = centerbox><p class = center-block-text>Thanks for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
+	text: '<div class = centerbox><p class = center-block-text>Thanks for playing!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
 	cont_key: [13],
 	timing_post_trial: 0,
 	on_finish: assessPerformance
@@ -280,7 +331,7 @@ var start_practice_block = {
 		trial_id: "practice_intro"
 	},
 	timing_response: 180000,
-	text: '<div class = centerbox><p class = block-text>We will start with a few practice trials. Remember, press the key corresponding to the <strong>ink</strong> color of the word: "r" for words colored red, "b" for words colored blue, and "g" for words colored green.</p><p class = block-text>Press <strong>enter</strong> to begin practice.</p></div>',
+	text: '<div class = centerbox><p class = block-text>Let\'s start with a few practice trials. Remember, press the key corresponding to the <strong><u>font color</u></strong> of the word. </p><p class = block-text></p><p class = block-text>Press <strong>enter</strong> to begin the practice.</p></div>',
 	cont_key: [13],
 	timing_post_trial: 1000
 };
@@ -291,7 +342,7 @@ var start_test_block = {
 		trial_id: "test_intro"
 	},
 	timing_response: 180000,
-	text: '<div class = centerbox><p class = center-block-text>We will now start the test. Respond exactly like you did during practice.</p><p class = center-block-text>Press <strong>enter</strong> to begin the test.</p></div>',
+	text: '<div class = centerbox><p class = center-block-text>Great job! Now that you\'ve had a bit of practice, let\'s play for real this time. Remember to respond as <u><strong>quickly and accurately</strong></u> as you can. </p><p class = center-block-text>Press <strong>enter</strong> to begin the test.</p></div>',
 	cont_key: [13],
 	timing_post_trial: 1000,
 	on_finish: function() {
@@ -324,18 +375,20 @@ for (i = 0; i < practice_len; i++) {
 	stroop_experiment.push(fixation_block)
 	var practice_block = {
 		type: 'poldrack-categorize',
+		practice_trial: i,
 		stimulus: practice_stims.stimulus[i],
 		data: practice_stims.data[i],
 		key_answer: practice_stims.key_answer[i],
 		is_html: true,
-		correct_text: '<div class = fb_box><div class = center-text><font size = 20>Correct!</font></div></div>',
-		incorrect_text: '<div class = fb_box><div class = center-text><font size = 20>Incorrect</font></div></div>',
-		timeout_message: '<div class = fb_box><div class = center-text><font size = 20>Respond Faster!</font></div></div>',
+		correct_text: '<div class = fb_box><div class = center-text><font size = 20>correct</font></div></div>',
+		incorrect_text: '<div class = fb_box><div class = center-text><font size = 20>WRONG!</font></div></div>',
+		timeout_message: '<div class = fb_box><div class = center-text><font size = 20>GO FASTER!</font></div></div>',
 		choices: choices,
 		timing_response: 1500,
 		timing_stim: -1,
 		timing_feedback_duration: 500,
 		show_stim_with_feedback: true,
+		response_ends_trial: true,
 		timing_post_trial: 250,
 		on_finish: function() {
 			jsPsych.data.addDataToLastTrial({
@@ -346,8 +399,6 @@ for (i = 0; i < practice_len; i++) {
 	}
 	stroop_experiment.push(practice_block)
 }
-stroop_experiment.push(attention_node)
-
 
 stroop_experiment.push(start_test_block)
 	/* define test trials */
@@ -359,14 +410,15 @@ for (i = 0; i < exp_len; i++) {
 		data: test_stims.data[i],
 		key_answer: test_stims.key_answer[i],
 		is_html: true,
-		correct_text: '<div class = fb_box><div class = center-text><font size = 20>Correct!</font></div></div>',
-		incorrect_text: '<div class = fb_box><div class = center-text><font size = 20>Incorrect</font></div></div>',
-		timeout_message: '<div class = fb_box><div class = center-text><font size = 20>Respond Faster!</font></div></div>',
+		correct_text: '<div class = fb_box><div class = center-text><font size = 20>correct</font></div></div>',
+		incorrect_text: '<div class = fb_box><div class = center-text><font size = 20>WRONG!</font></div></div>',
+		timeout_message: '<div class = fb_box><div class = center-text><font size = 20>GO FASTER!</font></div></div>',
 		choices: choices,
 		timing_response: 1500,
 		timing_stim: -1,
 		timing_feedback_duration: 500,
 		show_stim_with_feedback: true,
+		response_ends_trial: true,
 		timing_post_trial: 250,
 		on_finish: function() {
 			jsPsych.data.addDataToLastTrial({
@@ -376,7 +428,6 @@ for (i = 0; i < exp_len; i++) {
 		}
 	}
 	stroop_experiment.push(test_block)
+	if(i == exp_len/2) stroop_experiment.push(attention_node)
 }
-stroop_experiment.push(attention_node)
-stroop_experiment.push(post_task_block)
 stroop_experiment.push(end_block)
