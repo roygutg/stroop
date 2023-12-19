e8699b79d0545bddc0974dc74797a8476035d681
Roy Gutglick <roygutg@gmail.com>
2023-12-19 19:56:07 +0200

    instructions mention attention checks

--- experiment.js
+++ experiment.js
@@ -336,7 +336,8 @@ var start_test_block = {
     timing_response: 180000,
     text: `<div class = centerbox><p class = center-block-text>Great job! Now that you've had a bit of practice, you 
             can start the task. Remember to respond as <u><strong>quickly and accurately</strong></u> as you can.</p>
-            <p class = center-block-text>Press <strong>space</strong> to begin.</p>`,
+            <p class = center-block-text>Please keep your attention focused on the task. Attention checks will appear throughout.</p>
+            <p class = center-block-text>Press <strong>space</strong> to begin.</p></div>`,
     cont_key: [SPACE],
     timing_post_trial: 1000,
     on_finish: function () {
